cb3d479fc7eb4c5abd3d64e0d2b6a50dcf3cb127
vukhanh732 <vukhanh732@DESKTOP-HVJUMOE>
2024-04-10 23:27:33 +1000

    First commit

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,38 +19,26 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming, and along the way, I've managed to learn a thing or two, I believe… 🤷‍♂️
               <br />
-              <br />I am fluent in classics like
+              <br />I'm well-versed in the pillars of backend development, with a particular affinity for
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Java, C++, and Python.</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My passion lies in the art of building robust <i><b className="purple">web technologies and innovative products</b></i>, with a special interest in the burgeoning field of
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
+                <b className="purple">Artificial Intelligence.</b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Driven by curiosity and the endless possibilities of technology, I often find myself leveraging
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+                <b className="purple">AI and automation</b>
+              </i> to solve complex problems, aiming to streamline processes and enhance efficiency. ChatGPT and similar AI tools are my go-to companions in coding, enabling me to push the boundaries of what's possible in backend development.
             </p>
+
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
+          "IT Support Specialist",
           "Open Source Contributor",
         ],
         autoStart: true,
